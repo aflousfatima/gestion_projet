e17be286db285d794f646a9ea4d85c8683e4e62b
fatima aflous <aflousfatima@gmail.com>
2025-03-24 23:14:45 +0000

    manage autorizations

---
+++ Authentification_Service/src/main/java/com/auth/authentification_service/Configuration/SecurityConfig.java
@@ -0,0 +1,69 @@
+package com.auth.authentification_service.Configuration;
+
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true) // Active @PreAuthorize
+public class SecurityConfig {
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors().and()  // Assurez-vous que CORS est activé
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/signup","/api/login","/api/refresh").permitAll() // Endpoint public pour login
+                        .requestMatchers("/api/invitations").hasRole("MANAGER") // Réservé aux managers
+                        .requestMatchers("/api/me","/api/user-id").authenticated() // Accessible aux authentifiés
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt
+                                .jwtAuthenticationConverter(jwtAuthenticationConverter())
+                        )
+                        .authenticationEntryPoint((request, response, authException) -> {
+                            System.out.println("Échec d'authentification : " + authException.getMessage());
+                            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Token invalide ou non autorisé");
+                        })
+                );
+
+
+        return http.build();
+    }
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter(jwt -> {
+            List<String> authorities = new ArrayList<>();
+            Map<String, Object> realmAccess = jwt.getClaimAsMap("realm_access");
+            if (realmAccess != null && realmAccess.containsKey("roles")) {
+                List<String> roles = (List<String>) realmAccess.get("roles");
+                authorities.addAll(roles.stream().map(role -> "ROLE_" + role).collect(Collectors.toList()));
+            }
+            System.out.println("Authorities extraites : " + authorities);
+            return authorities.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+        });
+        return converter;
+    }
+
+
+}
