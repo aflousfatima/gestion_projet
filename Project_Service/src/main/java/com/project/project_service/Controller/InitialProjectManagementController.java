d87edbd60cb1987552a134f0dacc20a0fa32fcee
fatima aflous <aflousfatima@gmail.com>
2025-03-24 12:52:36 +0000

    add endpoint to fetch manager's infos

--- Project_Service/src/main/java/com/project/project_service/Controller/InitialProjectManagementController.java
+++ Project_Service/src/main/java/com/project/project_service/Controller/InitialProjectManagementController.java
@@ -1,6 +1,7 @@
 package com.project.project_service.Controller;
 
 import com.project.project_service.DTO.InitialProjectManagementDTO;
+import com.project.project_service.DTO.ProjectResponseDTO;
 import com.project.project_service.Service.InitialProjectManagementService;
 import com.project.project_service.config.AuthClient;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 public class InitialProjectManagementController {
 
     @Autowired
-    private InitialProjectManagementService companyTeamProjectService;
+    private InitialProjectManagementService initialProjectManagementService;
 
     @Autowired
     private AuthClient authClient;  // Injecter le client Auth pour extraire l'ID de l'utilisateur
@@ -31,7 +32,7 @@ public class InitialProjectManagementController {
 
             // Passer l'ID du créateur au service pour l'utiliser lors de la création des entités
             System.out.println("Appel au service pour créer l'entreprise, l'équipe et le projet...");
-            companyTeamProjectService.createCompanyTeamProject(dto, authId);
+            initialProjectManagementService.createCompanyTeamProject(dto, authId);
 
             System.out.println("Entreprise, équipe et projet créés avec succès.");
             return ResponseEntity.ok("Entreprise, équipe et projet créés avec succès.");
@@ -39,6 +40,17 @@ public class InitialProjectManagementController {
             System.out.println("Erreur lors de la création des entités : " + e.getMessage());
             e.printStackTrace();
             return ResponseEntity.status(500).body("Erreur lors de la création des entités.");
+        }
+    }
+
+
+    @GetMapping("/projects/by-manager")
+    public ResponseEntity<ProjectResponseDTO> getProjectsByManager(@RequestParam String authId) {
+        try {
+            ProjectResponseDTO response = initialProjectManagementService.getProjectsByManager(authId);
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            return ResponseEntity.status(400).body(null);
         }
     }
 }
