77f5a9649d1197eca12822e7e53df701a82c7710
fatima aflous <aflousfatima@gmail.com>
2025-04-12 04:58:02 +0100

    add a method to fetch user stories of an active sprint

--- Project_Service/src/main/java/com/project/project_service/Repository/SprintRepository.java
+++ Project_Service/src/main/java/com/project/project_service/Repository/SprintRepository.java
@@ -2,10 +2,14 @@ package com.project.project_service.Repository;
 
 import com.project.project_service.Entity.Projet;
 import com.project.project_service.Entity.Sprint;
+import com.project.project_service.Enumeration.SprintStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface SprintRepository extends JpaRepository<Sprint, Long> {
     List<Sprint> findByProject(Projet project);
+
+    Sprint findByProjectIdAndStatus(Long projectId, SprintStatus status);
+
 }
--- Project_Service/src/main/java/com/project/project_service/Repository/UserStoryRepository.java
+++ Project_Service/src/main/java/com/project/project_service/Repository/UserStoryRepository.java
@@ -3,10 +3,14 @@ package com.project.project_service.Repository;
 import com.project.project_service.Entity.Projet;
 import com.project.project_service.Entity.UserStory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface UserStoryRepository extends JpaRepository<UserStory, Long> {
 
     List<UserStory> findByProject(Projet projet);
+
+    List<UserStory> findBySprintId(Long sprintId);  // Méthode de base
 }
