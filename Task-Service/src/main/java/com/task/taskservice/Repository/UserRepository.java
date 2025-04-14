c638b785f04b06ad15b3177304df08e101a442e2
fatima aflous <aflousfatima@gmail.com>
2025-04-14 02:39:17 +0100

    update kanban view

--- Task-Service/src/main/java/com/task/taskservice/Repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.task.taskservice.Repository;
-
-
-import com.task.taskservice.Entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
