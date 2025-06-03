bdf75bbb2c7ff950938cc4cb4f751c44bfaf241a
fatima aflous <aflousfatima@gmail.com>
2025-06-03 13:03:26 +0100

    test mlops workflow

--- AI_Service/auth_gdrive.py
+++
@@ -1,8 +0,0 @@
-from pydrive2.auth import GoogleAuth
-
-gauth = GoogleAuth()
-gauth.settings['client_config_file'] = "client_secrets.json"
-gauth.LocalWebserverAuth()  # Cela ouvre un navigateur pour l'authentification
-
-gauth.SaveCredentialsFile("credentials.json")  # Sauvegarde le token valide dans ce fichier
-print("Token d'authentification sauvegardé dans credentials.json")
