971ab686ae63b26d8865411fdc7dc263d901af0d
fatima aflous <aflousfatima@gmail.com>
2025-06-14 13:53:56 +0100

    delete some files

--- .in-toto/check_key.py
+++
@@ -1,7 +0,0 @@
-from securesystemslib import keys
-
-with open("private.key", "r") as f:
-    private_key_data = f.read()
-
-rsa_key = keys.import_rsakey_from_private_pem(private_key_data)
-print(f"KeyID: {rsa_key['keyid']}")
